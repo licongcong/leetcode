b6ad7de7e876f9532da275c77d7df76fbfc661aa
Congcong Li <ling873495101@gmail.com>
2016-11-15 10:37:13 +0800

    sqrt

---
+++ algorithms/cpp/sqrtX/sqrt_x.cc
@@ -0,0 +1,21 @@
+// Copyright 2016 <lee>
+// Author: Congcong Li
+// Mail: ling873495101@gmail.com
+// Time: Tue Nov 15 10:35:24 2016
+// Source: https://leetcode.com/problems/sqrtx/
+
+// Implement int sqrt(int x).
+// Compute and return the square root of x.
+
+class Solution {
+ public:
+  int mySqrt(int x) {
+    int root = 1 << 16;
+
+    while (pow(root, 2) >= x) {
+      root = (x/root+root) >> 1;
+      if (pow(root, 2) <= x) break;
+    }
+    return root;
+  }
+};
