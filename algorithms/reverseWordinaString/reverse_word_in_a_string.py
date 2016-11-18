64d69bc238bfcd49b4282fc7bdccec07dae38677
Congcong Li <ling873495101@gmail.com>
2016-11-18 11:44:02 +0800

    reverse word in a string

---
+++ algorithms/reverseWordinaString/reverse_word_in_a_string.py
@@ -0,0 +1,24 @@
+# Copyright 2016 <lee>
+# Author: Congcong Li
+# Mail: ling873495101@gmail.com
+# Time: Fri Nov 18 11:41:02 2016
+# Source: https://leetcode.com/problems/reverse-words-in-a-string/
+
+# Given an input string, reverse the string word by word.
+
+# For example,
+# Given s = "the sky is blue",
+# return "blue is sky the".
+
+# Update (2015-02-12):
+# For C programmers: Try to solve it in-place in O(1) space.
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        arr = s.split()
+        return ' '.join(arr[::-1])
+
