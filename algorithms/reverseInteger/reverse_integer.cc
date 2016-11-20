1d59310c0afab4a8c163f92db44b5945e462dd48
Congcong Li <ling873495101@gmail.com>
2016-11-20 10:27:22 +0800

    reverse integer

---
+++ algorithms/reverseInteger/reverse_integer.cc
@@ -0,0 +1,23 @@
+// Copyright 2016 <lee>
+// Author: Congcong Li
+// Mail: ling873495101@gmail.com
+// Time: Sun Nov 20 10:19:56 2016
+// Source: https://leetcode.com/problems/reverse-integer/
+
+// Reverse digits of an integer.
+
+// Example1: x = 123, return 321
+// Example2: x = -123, return -321
+
+class Solution {
+ public:
+  int reverse(int x) {
+    // takes care of overflow
+    long long ans = 0LL;
+    while (x) {
+      ans = ans*10 + x%10;
+      x /= 10;
+    }
+    return (ans > INT_MAX || ans < INT_MIN) ? 0 : ans;
+  }
+};
