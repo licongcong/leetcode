9fe0947c7fe6ad085e3f406705990812aa726efc
Congcong Li <ling873495101@gmail.com>
2016-11-23 21:36:28 +0800

    product of array except self

---
+++ algorithms/productofArrayExceptSelf/product_of_array_except_self.cc
@@ -0,0 +1,40 @@
+// Copyright 2016 <lee>
+// Author: Congcong Li
+// Mail: ling873495101@gmail.com
+// Time: Wed Nov 23 21:31:49 2016
+// Source: https://leetcode.com/problems/product-of-array-except-self/
+
+// Given an array of n integers where n > 1, nums, return an array output such
+// that output[i] is equal to the product of all the elements of nums except
+// nums[i].
+
+// Solve it without division and in O(n).
+
+// For example, given [1,2,3,4], return [24,12,8,6].
+
+// Follow up:
+// Could you solve it with constant space complexity?
+// (Note: The output array does not count as extra space for the purpose of
+// space complexity analysis.)
+
+class Solution {
+ public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> result{1};
+    // result[1] = 1
+    // result[2] = 1 * nums[1]
+    // result[3] = 1 * nums[1] * nums[2]
+    // result[4] = 1 * nums[1] * nums[2] * nums[3];
+    // ...
+    for (int i=0; i < nums.size()-1; i++) {
+      result.push_back(result[i] * nums[i]);
+    }
+
+    int temp = 1;
+    for (int i=result.size()-1; i >= 0; i--) {
+      result[i] *= temp;
+      temp *= nums[i];
+    }
+    return result;
+  }
+};
