7d0880f8370ce7dd3526f5eb9af1ea0b77dd6d16
Congcong Li <ling873495101@gmail.com>
2016-12-04 09:02:07 +0800

    119. Pascal's Triangle II

---
+++ algorithms/pascalsTriangleII/pascals_triangle_ii.cc
@@ -0,0 +1,30 @@
+// Copyright 2016 <lee>
+// Author: Congcong Li
+// Mail: ling873495101@gmail.com
+// Time: Sun Dec  4 09:00:27 2016
+// Source: https://leetcode.com/problems/pascals-triangle-ii/
+
+// Given an index k, return the kth row of the Pascal's triangle.
+
+// For example, given k = 3,
+// Return [1,3,3,1].
+
+// Note:
+// Could you optimize your algorithm to use only O(k) extra space?
+
+class Solution {
+ public:
+  vector<int> getRow(int rowIndex) {
+    vector<int> result(1, 1);
+    vector<int> temp(result);
+    for (int i=1; i <= rowIndex; i++) {
+      for (int j=1; j < i; j++) {
+        temp.push_back(result[j-1]+result[j]);
+      }
+      temp.push_back(1);
+      result = temp;
+      temp.resize(1);
+    }
+    return result;
+  }
+};
