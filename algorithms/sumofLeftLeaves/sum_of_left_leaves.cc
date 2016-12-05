3f67316d108c29bc444cd2cbc7358b658c69828f
Congcong Li <ling873495101@gmail.com>
2016-12-05 10:58:55 +0800

    404. Sum of Left Leaves

---
+++ algorithms/sumofLeftLeaves/sum_of_left_leaves.cc
@@ -0,0 +1,37 @@
+// Copyright 2016 <lee>
+// Author: Congcong Li
+// Mail: ling873495101@gmail.com
+// Time: Mon Dec  5 10:55:02 2016
+// Source: https://leetcode.com/problems/sum-of-left-leaves/
+
+// Find the sum of all left leaves in a given binary tree.
+
+// Example:
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+
+// There are two left leaves in the binary tree, with values 9 and 15
+// respectively. Return 24.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  int sumOfLeftLeaves(TreeNode* root) {
+    if (!root) return 0;
+    if (root->left && (!root->left->left && !root->left->right))
+      return root->left->val + sumOfLeftLeaves(root->right);
+    return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+  }
+};
