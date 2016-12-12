e308537342d5dacc31ad6da6f951a21dd4efad5b
Congcong Li <ling873495101@gmail.com>
2016-12-12 22:04:18 +0800

    28. Implement strStr()

---
+++ algorithms/implementStrStr/implement_strstr.cc
@@ -0,0 +1,32 @@
+// Copyright 2016 <lee>
+// Author: Congcong Li
+// Mail: ling873495101@gmail.com
+// Time: Mon Dec 12 22:02:57 2016
+// Source: https://leetcode.com/problems/implement-strstr/
+
+// Implement strStr().
+
+// Returns the index of the first occurrence of needle in haystack, or -1 if
+// needle is not part of haystack.
+
+class Solution {
+ public:
+  int strStr(string haystack, string needle) {
+    int i = 0;
+    int j = 0;
+    while (i < haystack.size() && j < needle.size()) {
+      if (haystack[i] == needle[j]) {
+        i++;
+        j++;
+      } else {
+        i = i - j + 1;
+        j = 0;
+      }
+    }
+    if (j == needle.size()) {
+      return i - j;
+    } else {
+      return -1;
+    }
+  }
+};
