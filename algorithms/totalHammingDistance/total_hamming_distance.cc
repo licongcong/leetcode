82f99d548392297cee3cde7d368a208ba48e2edb
Congcong Li <ling873495101@gmail.com>
2017-01-03 10:33:52 +0800

    477. Total Hamming Distance

---
+++ algorithms/totalHammingDistance/total_hamming_distance.cc
@@ -0,0 +1,45 @@
+// Copyright 2016 <lee>
+// Author: Congcong Li
+// Mail: licongcongcn@gmail.com
+// Time: Tue Jan  3 10:32:12 2017
+// Source: https://leetcode.com/problems/total-hamming-distance/
+
+// The Hamming distance between two integers is the number of positions at which
+// the corresponding bits are different.
+
+// Now your job is to find the total Hamming distance between all pairs of the
+// given numbers.
+
+// Example:
+// Input: 4, 14, 2
+
+// Output: 6
+
+// Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is
+// 0010 (just showing the four bits relevant in this case). So the answer will
+// be:
+// HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) =
+// 2 + 2 + 2 = 6.
+// Note:
+// Elements of the given array are in the range of 0 to 10^9
+// Length of the array will not exceed 10^4.
+
+class Solution {
+ public:
+  int totalHammingDistance(vector<int>& nums) {
+    int size = nums.size();
+    int total = 0;
+    int temp;
+    for (int i=0; i < 32; i++) {
+      int one = 0;
+      for (int j=0; j < nums.size(); j++) {
+        if (nums[j]&1) {
+          one++;
+        }
+        nums[j] = nums[j] >> 1;
+      }
+      total += one * (size - one);
+    }
+    return total;
+  }
+};
