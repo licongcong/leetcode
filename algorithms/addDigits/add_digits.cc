2eb6c340cc8f72fad5de290b760fff98e9abb345
Congcong Li <ling873495101@gmail.com>
2017-01-26 20:18:36 +0800

    258. Add Digits

---
+++ algorithms/addDigits/add_digits.cc
@@ -0,0 +1,28 @@
+// Copyright 2016 <lee>
+// Author: Congcong Li
+// Mail: licongcongcn@gmail.com
+// Time: Thu Jan 26 20:17:09 2017
+// Source: https://leetcode.com/problems/add-digits/
+
+// Given a non-negative integer num, repeatedly add all its digits until the
+// result has only one digit.
+
+// For example:
+
+// Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only
+// one digit, return it.
+
+// Follow up:
+// Could you do it without any loop/recursion in O(1) runtime?
+
+// Credits:
+// Special thanks to @jianchao.li.fighter for adding this problem and creating
+// all test cases.
+
+class Solution {
+ public:
+  int addDigits(int num) {
+    return 1 + (num - 1) % 9;
+  }
+};
+
